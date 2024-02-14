72906adee43bedb609f70db6791f459174982838
Sam Tanner <samuel.m.s.tanner@gmail.com>
2024-02-13 16:38:34 -0800

    adds export for with computedProps

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+export { withComputedProps } from "./functions/componentPropsFunction"
 export { useMakUi, MakUiProvider } from "./context/useMakUi"
 export * from "./types/index"
 export { default as mak } from "./elements/mak"
